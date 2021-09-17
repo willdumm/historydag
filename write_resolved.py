50f3b282678416de5d3670d20c5c93f22e2f4e23
Will Dumm <wrhdumm@gmail.com>
2021-09-17 09:21:11 -0700

    Real First Commit

---
+++ write_resolved.py
@@ -0,0 +1,7 @@
+import pickle
+from gctree import phylip_parse as pps
+
+treelist = pps.parse_outfile('outfile', root="GL")
+with open('resolvedtrees.p', 'wb') as fh:
+    fh.write(pickle.dumps(treelist))
+
