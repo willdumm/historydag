7e7db61684fb914d6c22f44961d16e048e751db5
Will Dumm <wrhdumm@gmail.com>
2021-12-19 07:53:01 -0800

    cleanup

--- historydag/write_resolved.py
+++
@@ -1,6 +0,0 @@
-import pickle
-from gctree import phylip_parse as pps
-
-treelist = pps.parse_outfile("outfile", root="GL")
-with open("resolvedtrees.p", "wb") as fh:
-    fh.write(pickle.dumps(treelist))
