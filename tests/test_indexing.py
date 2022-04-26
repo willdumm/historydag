e6d6470781c34d4b2f53f8b9d638ab1a9bd50d79
Claris <clarisw@cs.washington.edu>
2022-04-26 11:30:51 -0700

    wrote first test and formatter

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -1,21 +1,13 @@
-from historydag.dag import (
-    history_dag_from_newicks,
-)
+from historydag.dag import history_dag_from_newicks
 
 newicklistlist = [
-    [
-        "((AA, CT)CG, (TA, CC)CG)CC;",
-        "((AA, CT)CA, (TA, CC)CC)CC;",
-    ],
+    ["((AA, CT)CG, (TA, CC)CG)CC;", "((AA, CT)CA, (TA, CC)CC)CC;",],
     [
         "((CA, GG)CA, AA, (TT, (CC, GA)CC)CC)AA;",
         "((CA, GG)CA, AA, (TT, (CC, GA)CA)CA)AA;",
         "((CA, GG)CG, AA, (TT, (CC, GA)GC)GC)AG;",
     ],
-    [
-        "((AA, CT)CG, (TA, CC)CG)CC;",
-        "((AA, CT)CA, (TA, CC)CC)CC;",
-    ],
+    ["((AA, CT)CG, (TA, CC)CG)CC;", "((AA, CT)CA, (TA, CC)CC)CC;",],
 ]
 
 
@@ -23,21 +15,26 @@ newicklistlist = [
 # captures all possible subtrees of the dag
 # (this is the dag defined by newicklistlist)
 def test_indexing_comprehensive():
-    dags = [
+    # dags = [
+    #     history_dag_from_newicks(
+    #         newicklist, [], label_functions={"sequence": lambda n: n.name}
+    #     )
+    #     for newicklist in newicklistlist
+    # ]
+    history_dag = [
         history_dag_from_newicks(
             newicklist, [], label_functions={"sequence": lambda n: n.name}
         )
         for newicklist in newicklistlist
-    ]
-    history_dag = history_dag[1]
+    ][1]
     # get the set of all dags that were indexed
-    all_dags_indexed = {None} # set of all the indexed dags
+    all_dags_indexed = {None}  # set of all the indexed dags
     curr_dag_index = 0
-    while not history_dag[curr_dag_index] == None:
+    while not history_dag[curr_dag_index] is None:
         next_tree = history_dag[curr_dag_index]
         curr_dag_index = curr_dag_index + 1
         all_dags_indexed.add(next_tree)
-    
+
     # get the set of all dags from the get_trees
     all_dags_true_generator = history_dag.get_trees()
     all_dags_true = {None}
